a0fa105018e376e0540c83888cbf4db77d8e93a4
DmitrievDV <13@13.tt>
2020-07-16 12:31:38 +0300

    Add project files.

---
+++ ChaosGameFractals/Figures.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpGL.SceneGraph;
+namespace ChaosGameFractals
+{
+    class Figures
+    {     
+        static double PI = Math.PI;
+
+        public static Vertex[] Triangle = new Vertex[]
+        {
+            new Vertex(-10,-10,0),
+            new Vertex(10,-10,0),
+            new Vertex(0,10,0)
+        };
+
+        public static Vertex[] Square = new Vertex[] 
+        { 
+            new Vertex(-10,-10,0),
+            new Vertex(10,-10,0),
+            new Vertex(-10,10,0),
+            new Vertex(10,10,0),
+        };
+   
+        public static Vertex[] Pentagon = new Vertex[] 
+        { 
+            new Vertex(10 * (float)Math.Cos(0),10 * (float)Math.Sin(0),0),
+            new Vertex(10 * (float)Math.Cos(2*PI/5),10 * (float)Math.Sin(2*PI/5),0),
+            new Vertex(10 * (float)Math.Cos(4*PI/5),10 * (float)Math.Sin(4*PI/5),0),
+            new Vertex(10 * (float)Math.Cos(6*PI/5),10 * (float)Math.Sin(6*PI/5),0),
+            new Vertex(10 * (float)Math.Cos(8*PI/5),10 * (float)Math.Sin(8*PI/5),0)
+        };
+
+        public static Vertex[] Hexagon = new Vertex[] 
+        { 
+            new Vertex(10 * (float)Math.Cos(0),10 * (float)Math.Sin(0),0),
+            new Vertex(10 * (float)Math.Cos(2*PI/6),10 * (float)Math.Sin(2*PI/6),0),
+            new Vertex(10 * (float)Math.Cos(4*PI/6),10 * (float)Math.Sin(4*PI/6),0),
+            new Vertex(10 * (float)Math.Cos(6*PI/6),10 * (float)Math.Sin(6*PI/6),0),
+            new Vertex(10 * (float)Math.Cos(8*PI/6),10 * (float)Math.Sin(8*PI/6),0),
+            new Vertex(10 * (float)Math.Cos(10*PI/6),10 * (float)Math.Sin(10*PI/6),0),
+        };
+
+
+        public static Vertex[] GetFigure(int VertNum, float R = 10, float Phi = 0)
+        {
+            if (VertNum < 3)
+                VertNum = 3;
+            Vertex[] V = new Vertex[VertNum];
+            Double d = PI/VertNum;
+            for (int i = 0; i < VertNum; i++)
+            {
+                V[i] = new Vertex(
+                    x: R * (float)Math.Cos(2*i*d + Phi),
+                    y: R * (float)Math.Sin(2*i*d + Phi),
+                    z: 0
+                    );
+            }
+            return V;
+        }
+    }
+}
---
+++ ChaosGameFractals/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using SharpGL.WPF;
+using SharpGL;
+using SharpGL.Enumerations;
+using SharpGL.SceneGraph;
+namespace ChaosGameFractals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public enum Restriction
+    {
+        NoRestriction,
+        UniqueNeighbours,
+        Not2PlacesAway,
+    }
+    public partial class MainWindow : Window
+    {
+        OpenGLControl GLControl;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GLControl = new OpenGLControl();
+            GLControl.FrameRate = 60;
+
+            MainGrid.Children.Add(GLControl);
+
+            GLControl.OpenGLInitialized += GL_INIT;
+            GLControl.Resized += GLControl_Resized;
+            GLControl.OpenGLDraw += ChaosGame;
+
+            FractalsCB.ItemsSource = Enumerable.Range(3, 10);
+            FractalsCB.SelectedItem = 3;
+        }
+
+        void GLControl_Resized(object sender, OpenGLEventArgs args)
+        {
+            OpenGL GL = args.OpenGL;
+            GL.Clear(OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_COLOR_BUFFER_BIT);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho2D(
+                left: -15.0f,
+                right: 15.0f,
+                bottom: -15.0f,
+                top: 15.0f);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+
+        void GL_INIT(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            OpenGL GL = args.OpenGL;
+
+            GL.ClearColor(0f, 0f, 0f, 0f);
+            GL.ShadeModel(ShadeModel.Smooth);
+            GL.Enable(OpenGL.GL_BLEND);
+            GL.BlendFunc(BlendingSourceFactor.SourceAlpha, BlendingDestinationFactor.OneMinusSourceAlpha);
+            GL.Enable(OpenGL.GL_POINT_SMOOTH);
+            GL.Color(0.3f, 1f, 0.3f, 0.1f);
+            GL.PointSize(2f);
+            GL.LineWidth(3f);
+            Point = new Vertex(r.Next(), r.Next(), r.Next());
+        }
+        Vertex[] Figure;
+        Restriction Restr = Restriction.NoRestriction;
+       static Random r = new Random();
+        Vertex Point;
+        int lastvert = 0;
+        int VertN;
+        void ChaosGame(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            OpenGL GL = GLControl.OpenGL;
+            GL.Clear(OpenGL.GL_DEPTH_BUFFER_BIT);
+            GL.LoadIdentity();           
+            int iterations = 5000;
+            GL.Begin(BeginMode.Points);
+            for (int i = 0; i < iterations; i++)
+            {
+                VertN = GetVertNumber(Figure.Length, lastvert, Restr);
+                lastvert = VertN;
+                Point = (Point + Figure[VertN]) * (float)Fraction.Value;
+                GL.Vertex(Point);
+            }
+            GL.End();
+        }
+        int GetVertNumber(int N, int Last, Restriction R)
+        {
+           
+            int v = r.Next(0,N);
+            switch (R)
+            {
+                case Restriction.NoRestriction:
+                    return v;
+
+                case Restriction.UniqueNeighbours:
+                    while (Last == v)
+                        v = r.Next(0, N);
+                    return v;
+
+                case Restriction.Not2PlacesAway:
+                    while (Math.Abs(Last-v) == 2)
+                        v = r.Next(0, N);
+                    return v;
+
+                default: return v;
+            }
+
+        }
+        private void Fraction_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            OpenGL GL = GLControl.OpenGL;
+            GL.Clear(OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_COLOR_BUFFER_BIT);
+            GL.LoadIdentity();
+        }
+
+        private void FractalsCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OpenGL GL = GLControl.OpenGL;
+            GL.Clear(OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_COLOR_BUFFER_BIT);
+            GL.LoadIdentity();
+
+
+            Figure = Figures.GetFigure(
+                VertNum: (int)e.AddedItems[0],
+                Phi: phirad
+                );
+        }
+
+
+        float phirad = 0;
+        private void PhiAngle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            OpenGL GL = GLControl.OpenGL;
+            GL.Clear(OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_COLOR_BUFFER_BIT);
+            GL.LoadIdentity();
+
+            phirad = (float)(Math.PI / 180 * e.NewValue);
+
+            Figure = Figures.GetFigure(
+                VertNum: (int)FractalsCB.SelectedItem,
+                Phi: phirad
+                );
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.IsChecked == true)
+            {
+                Restr = Restriction.UniqueNeighbours;
+            }
+            else
+            {
+                Restr = Restriction.NoRestriction;
+            }
+            OpenGL GL = GLControl.OpenGL;
+            GL.Clear(OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_COLOR_BUFFER_BIT);
+            GL.LoadIdentity();
+        }
+    }
+}
